3bf2f259d1d71a36519fce13c711315f25039d8b
martin107 <martin107@2659379.no-reply.drupal.org>
2015-12-14 07:18:03 -0500

    Issue #2494629 by martin107: getFormID method named incorrectly.

--- lib/Drupal/twitter/Form/TwitterSettingsForm.php
+++ lib/Drupal/twitter/Form/TwitterSettingsForm.php
@@ -18,7 +18,7 @@ class TwitterSettingsForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function getFormID() {
+  public function getFormId() {
     return 'twitter_settings_form';
   }
 
